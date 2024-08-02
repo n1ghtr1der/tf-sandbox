f10e5ea998705de8e4e14ab3fd7fb3120bf05287
Emirton Sousa <emirtonf@gmail.com>
2024-08-02 18:46:18 -0300

    build: infra added

--- variables.tf
+++ variables.tf
@@ -1,14 +1,53 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+#EC2
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "access_key_name" {
+  type = string
+  default = "dev-key"
+}
+
+variable "front_end_disk_size" {
+  type = number
+  default = 8
+}
+
+variable "back_end_disk_size" {
+  type = number
+  default = 8
+}
+
+variable "ami_id" {
+  type = string
+  default = "ami-0103953a003440c37"
+}
+
+variable "key_pair_pub_key" {
+  type = string
+}
+
+#Network
 variable "vpc_cidr_block" {
-    description = "VPC CIDR Block eg. 10.0.0.0/16, 172.16.0.0/16, 192.168.0.0/16"
-    type = string
+  type = string
+  default = "10.0.0.0/16"
 }
 
-variable "vpc_name" {
-    description = "VPC Name"
-    type = string
-}
-
-variable "vpc_owner" {
-    description = "VPC Owner Team"
-    type = string
+variable "subnet_cidr_block" {
+  type = string
+  default = "10.0.0.0/24"  
 }
--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,115 @@
-resource "aws_vpc" "ecorp_vpc" {
+resource "aws_vpc" "sandbox_vpc" {
   cidr_block = var.vpc_cidr_block
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    Owner: "${var.vpc_owner}"
-    Name: "${var.vpc_name}"
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "sandbox_igw" {
+  vpc_id = aws_vpc.sandbox_vpc.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_subnet" "sandbox_subnet" {
+  vpc_id = aws_vpc.sandbox_vpc.id
+  cidr_block = var.subnet_cidr_block
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "${var.project_name}-subnet"
+  }
+}
+
+resource "aws_route_table" "sandbox_route_table" {
+  vpc_id = aws_vpc.sandbox_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.sandbox_igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-route-table"
+  }
+}
+
+resource "aws_route_table_association" "sandbox_route_table_association" {
+  subnet_id = aws_subnet.sandbox_subnet.id
+  route_table_id = aws_route_table.sandbox_route_table.id 
+}
+
+resource "aws_security_group" "sandbox_front_sg" {
+  name = "${var.project_name}-front-end-sg"
+  description = "${var.project_name} front end security group"
+  vpc_id = aws_vpc.sandbox_vpc.id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-front-sg"
+  }
+}
+
+resource "aws_security_group" "sandbox_back_sg" {
+  name = "${var.project_name}-back-end-sg"
+  description = "${var.project_name} back end security group"
+  vpc_id = aws_vpc.sandbox_vpc.id
+
+  ingress {
+    from_port = 3333
+    to_port = 3333
+    protocol = "tcp"
+    cidr_blocks = ["${aws_vpc.sandbox_vpc.cidr_block}"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.sandbox_vpc.cidr_block}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-back-sg"
   }
 }
